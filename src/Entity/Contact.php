a6f0a264956f23d6b742eb7f88dc388975f05917
Laura Buaillon <laura.buaillon1@gmail.com>
2026-05-06 11:01:26 +0200

    entité contact

---
+++ src/Entity/Contact.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $is_active = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): static
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+}
